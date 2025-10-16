53b5dec5333d5f7fff5dc5c6813f19aeac4e2773
Annie574 <88533090+Annie574@users.noreply.github.com>
2025-10-16 12:07:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-SHEET_LINK = "https://forms.gle/77ebpiJJx4Rj4uut9"
+SHEET_LINK = <YOUR_LINK_HERE>
 ZILLOW_CLONE = "https://appbrewery.github.io/Zillow-Clone/"
 
 # Get the Website
-response = requests.get(ZILLOW_CLONE)
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+    "Accept-Language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7"
+}
+response = requests.get(ZILLOW_CLONE, headers=header)
 
 # Make a soup from Website
 soup = BeautifulSoup(response.text, "html.parser")
@@ -61,3 +65,4 @@ for i in range(len(list_of_addresses)):
 driver.quit()
 
 print(f"Saving data completed! Log in to Google and access link {SHEET_LINK} to view responses!")
+
